ac11c2ef28d7ca39bd646077660e95d233f17275
Ahrijin <1276548095@qq.com>
2019-01-23 14:55:28 +0800

    笨蛋李广强

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -5,16 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    String name;
-    String sex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //第一次
-        //第三次
-        //第二次
-        //李广强大帅哥
+
     }
 }
